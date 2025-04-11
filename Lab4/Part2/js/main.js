cdf8c0d560858023383650b183d5ff22a57fbd38
SebastianPineda88 <sebastian.pinedamedina@dcmail.ca>
2025-04-11 14:15:04 -0400

    extra changes
    extra

--- Lab4/Part2/js/main.js
+++ Lab4/Part2/js/main.js
@@ -1,4 +1,6 @@
 //alert("in main.js");
+
+
 const images = [
     "pic1.jpg",
     "pic2.jpg",
@@ -15,10 +17,42 @@ const images_alt_text = [
     "Butterfly on leaf"
     
 ]; 
+
+
 const thumbbar = document.querySelector(".thumb-bar")
 for (const image of images) {
     var elem = document.createElement("img");
     elem.setAttribute("src", "./images/"+image);
     elem.setAttribute("alt", images_alt_text[images.indexOf(image)]);
+    elem.addEventListener("click",() => alert("clicked");
     thumbbar.appendChild(elem);
+
 }
+
+
+
+const button = document.querySelector("button")
+button.addEventListener("click",() => darken(button))
+const over = document.querySelector(".overlay")
+function darken(btn){
+    buttonclass = btn.getAttribute("class")
+
+    if (buttonclass==="dark"){
+
+        btn.setAttribute("class", "light")
+        btn.textContent='Lighten'
+        over.style.background='rgba(0 0 0 / 50%)'
+        
+    }
+    else if (buttonclass ==="light"){
+
+        btn.setAttribute("class", "dark")
+        btn.textContent='Darken'
+        over.style.background='rgba(0 0 0 / 0%)'
+        
+    }
+
+    
+}
+
+
